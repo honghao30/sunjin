fde967dea894a49fde417ea264a68ed9f40ea40e
hongsh <hongsh@the-51.com>
2024-07-22 17:11:00 +0900

    페이지 구성~

--- src/views/insert/Insert.vue
+++ src/views/insert/Insert.vue
@@ -176,19 +176,162 @@
                         </div>
                     </el-tab-pane>
                     <el-tab-pane label="Vibrio" name="second">
-                        <div class="graph-area">
-                            그래프
-                        </div>
+                        <table class="table table-list">
+                            <colgroup>
+                                <col width="25%" />
+                                <col width="25%" />
+                                <col width="25%" />
+                                <col width="25%" />
+                            </colgroup>
+                            <thead>
+                                <tr>
+                                    <th scope="col">Type</th>
+                                    <th scope="col">Water</th>
+                                    <th scope="col">Liver</th>
+                                    <th scope="col">Gut </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>White</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>                                        
+                                </tr>
+                                <tr>
+                                    <td>Purple</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>     
+                                <tr>
+                                    <td>Blue</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>                                        
+                                </tr>   
+                                <tfoot>
+                                    <tr>
+                                        <td>Total</td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>                                        
+                                    </tr>  
+                                </tfoot>                                                                                                     
+                            </tbody>
+                        </table> 
                     </el-tab-pane>
                     <el-tab-pane label="Health" name="third">
-                        <div class="graph-area">
-                            그래프2
-                        </div>
+                        <table class="table table-list">
+                            <colgroup>
+                                <col width="25%" />
+                                <col width="25%" />
+                                <col width="25%" />
+                                <col width="25%" />
+                            </colgroup>
+                            <thead>
+                                <tr>
+                                    <th scope="col"></th>
+                                    <th scope="col">Bad</th>
+                                    <th scope="col">Normal </th>
+                                    <th scope="col">Good</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Liver </td>
+                                    <td><el-radio v-model="radio" label="1"><span class="blind">Bad</span></el-radio></td>
+                                    <td><el-radio v-model="radio" label="2"><span class="blind">Normal</span></el-radio></td>
+                                    <td><el-radio v-model="radio" label="3"><span class="blind">Good</span></el-radio></td>                                        
+                                </tr>
+                                <tr>
+                                    <td>Gut </td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>     
+                                <tr>
+                                    <td>reserved</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>                                        
+                                </tr>        
+                                <tr>
+                                    <td>reserved</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>                                        
+                                </tr>                                                                                                                              
+                            </tbody>
+                        </table>
                     </el-tab-pane>              
                     <el-tab-pane label="Photo" name="fourth">
-                        <div class="graph-area">
-                            그래프3
-                        </div>
+                        <table class="table table-body">
+                            <colgroup>
+                                <col width="33.3%" />
+                                <col width="33.3%" />
+                                <col width="*" />                                
+                            </colgroup>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Liver </th>
+                                    <td>0 picture(s) uploaded</td>
+                                    <td>
+                                        <el-upload
+                                            class="upload-demo"
+                                            action="https://jsonplaceholder.typicode.com/posts/"
+                                            :on-preview="handlePreview"
+                                            :on-remove="handleRemove"
+                                            :before-remove="beforeRemove"
+                                            multiple
+                                            :limit="3"
+                                            :on-exceed="handleExceed"
+                                            :file-list="fileList"
+                                        >
+                                            <el-button size="small" type="primary">Upload</el-button>                                            
+                                        </el-upload>                                        
+                                    </td>                                                                        
+                                </tr>
+                                <tr>
+                                    <th scope="row">Gut </th>
+                                    <td>0 picture(s) uploaded</td>
+                                    <td>
+                                        <el-upload
+                                            class="upload-demo"
+                                            action="https://jsonplaceholder.typicode.com/posts/"
+                                            :on-preview="handlePreview"
+                                            :on-remove="handleRemove"
+                                            :before-remove="beforeRemove"
+                                            multiple
+                                            :limit="3"
+                                            :on-exceed="handleExceed"
+                                            :file-list="fileList"
+                                        >
+                                            <el-button size="small" type="primary">Upload</el-button>                                            
+                                        </el-upload>                                         
+                                    </td>                                    
+                                </tr>     
+                                <tr>
+                                    <th scope="row">Whole</th>
+                                    <td>0 picture(s) uploaded</td>
+                                    <td>
+                                        <el-upload
+                                            class="upload-demo"
+                                            action="https://jsonplaceholder.typicode.com/posts/"
+                                            :on-preview="handlePreview"
+                                            :on-remove="handleRemove"
+                                            :before-remove="beforeRemove"
+                                            multiple
+                                            :limit="3"
+                                            :on-exceed="handleExceed"
+                                            :file-list="fileList"
+                                        >
+                                            <el-button size="small" type="primary">Upload</el-button>                                            
+                                        </el-upload>                                         
+                                    </td>                                                                          
+                                </tr>  
+                            </tbody>
+                        </table>
                     </el-tab-pane>                           
                 </el-tabs>
             </div>
@@ -197,7 +340,24 @@
                 <el-button type="primary">Save</el-button>
             </div>
 		</div>
-
+        <el-dialog            
+            :visible.sync="dialogVisible"
+            width="50%"
+        >
+        <div class="modal-content">
+            <div class="data-insert-form">
+                <div class="tit">Salinity</div>
+                <div class="input-wrap">
+                    <el-input-number v-model="num" controls-position="right" @change="handleChange" :min="1" :max="10"></el-input-number>                    
+                </div>
+            </div>
+        </div>
+        <span slot="footer" class="dialog-footer">
+            <el-button>Prev</el-button>
+            <el-button>Next</el-button>
+            <el-button type="primary" @click="dialogVisible = false">OK</el-button>
+        </span>
+        </el-dialog>
 	</div>
 </template>
 
@@ -211,9 +371,28 @@ const lastDate = ref('')
 const province = ref('')
 const activeName = ref('first')
 const SelectValue = ref('')
-const dialogVisible = ref(false)
+const dialogVisible = ref(true)
 const detailModal = ref(true)
+const radio = ref('1');
+const num = ref('2');
 
+const fileList = ref([
+    {name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, 
+    {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+)
+
+const handleRemove = (file, fileList) => {
+    console.log(file, fileList);
+}
+const handlePreview = (file) => {
+    console.log(file);
+}
+const handleExceed = (files, fileList) => {
+    this.$message.warning(`The limit is 3, you selected ${files.length} files this time, add up to ${files.length + fileList.length} totally`);
+}
+const beforeRemove = (file, fileList) => {
+    return this.$confirm(`Cancel the transfert of ${ file.name } ?`);
+}
 
 const handleClose = () => {  
     detailModal.value = false;
@@ -226,18 +405,6 @@ const handleClick = () => {
 const handleDetailView = () => {
     detailModal.value = true;
 }
-
-const swiperOption =  ref({
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction'
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-        // Other Swiper options
-});
 
 const options = [
     {
@@ -261,8 +428,6 @@ const options = [
         label: 'Option5',
     },
 ]
-
-
 </script>
 
 <style scoped>
