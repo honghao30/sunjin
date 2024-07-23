b3c7dddb658d97d5bc665b26fda073f1f38c0767
hongsh <hongsh@the-51.com>
2024-07-23 09:20:59 +0900

    로그인 페이지

--- src/views/layout/LoginLayout.vue
+++ src/views/layout/LoginLayout.vue
@@ -1,57 +1,11 @@
 <template>
-	<div class="parent">
-		<div class="logo">
-			<logo />
-		</div>
-		<div class="top-menu"></div>
-		<div class="main">
-			<router-view/>
-		</div>
-		<div class="footer">
-			<span>@2020 제주데이터허브 관리자 페이지</span>
-		</div>
+	<div class="login__wrap">
+		<router-view/>
 	</div>
 </template>
 
 <script>
-import Logo from "@/components/topmenu/Logo";
-
-export default {
-	name: "DefaultLayout",
-	components: {
-		Logo
-	}
-}
 </script>
 
 <style scoped>
-.parent {
-	display: grid;
-	grid-template-columns: 50px 150px 1fr;
-	grid-template-rows: 80px 1fr 50px;
-	grid-column-gap: 0px;
-	grid-row-gap: 0px;
-}
-
-.logo {
-	grid-area: 1 / 1 / 2 / 3;
-}
-
-.top-menu {
-	grid-area: 1 / 1 / 2 / 4;
-}
-
-.main {
-	grid-area: 2 / 1 / 3 / 4;
-}
-
-.footer {
-	grid-area: 3 / 1 / 4 / 4;
-	position: absolute;
-	left: 0;
-	bottom: 0;
-	width: 100%;
-	padding: 15px 0;
-	text-align: center;
-}
 </style>
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,23 +1,34 @@
 <template>
-	<div class="page-login">
-		<div id="container">
-			<div class="content-wrap">
-				<h1 class="form-title">로그인</h1>
-				<form class="form">
-					<div class="i-vertical">
-						<label for="email" style="text-align: left;">이메일</label>
-						<input type="email" id="email">
-					</div>
-					<div class="i-vertical">
-						<label for="password" style="text-align: left;">비밀번호</label>
-						<input type="password" id="password">
-					</div>
-					<div class="contents-footer">
-						<button type="submit" class="btn btn-fill-bluedark w-full" >로그인</button>
-					</div>
-				</form>
-			</div><!--//content-wrap-->
-		</div><!--//container-->
+	<div class="login-form__wrap">
+		<div class="login-page__logo">
+			<img src="../../assets/image/common/logo_color.svg" alt="" />
+		</div>
+		<div class="login__form">
+			<form>
+				<div class="input-item__wrap">
+					<i class="icon-io">
+						<svg xmlns="http://www.w3.org/2000/svg" width="11" height="10" viewBox="0 0 11 10" fill="none">
+							<path d="M10.4998 9.5H0.50025C0.524617 7.2867 2.32638 5.5 4.54545 5.5H6.45455C8.67362 5.5 10.4754 7.2867 10.4998 9.5Z" stroke="#D9D9D9"/>
+							<circle cx="5.5" cy="2.5" r="2" stroke="#D9D9D9"/>
+						</svg>
+					</i>
+					<el-input placeholder="userId" v-model="userId"></el-input>
+				</div>
+				<div class="input-item__wrap">
+					<i class="icon-io">
+						<svg xmlns="http://www.w3.org/2000/svg" width="11" height="13" viewBox="0 0 11 13" fill="none">
+							<rect x="0.5" y="4.73077" width="10" height="7.46154" rx="0.5" stroke="#D9D9D9"/>
+							<path d="M2.19231 3.80769C2.19231 1.98091 3.67321 0.5 5.5 0.5C7.32679 0.5 8.80769 1.98091 8.80769 3.80769V4.57692H2.19231V3.80769Z" stroke="#D9D9D9"/>
+							<circle cx="5.5" cy="8.73076" r="1.26923" fill="#C4C4C4"/>
+						</svg>
+					</i>					
+					<el-input placeholder="Password" v-model="passWord" show-password></el-input>
+				</div>
+				<div class="button__wrap">
+					<el-button type="primary">Login</el-button>
+				</div>
+			</form>
+		</div>
 	</div>
 </template>
 
@@ -26,7 +37,10 @@
 export default {
 	name: "Login",
 	data() {
-		return {};
+		return {
+			userId,
+			passWord,
+		};
 	},
 	methods: {}
 };
