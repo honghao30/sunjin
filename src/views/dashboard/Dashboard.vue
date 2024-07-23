ae404ec8ea57d857327cfc8c022adf93cf000c96
hongsh <hongsh@the-51.com>
2024-07-23 16:57:38 +0900

    대시보드~

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -2,30 +2,40 @@
 	<div class="wrap">
 		<div class="link-group">
 			<div class="logo">
-				<img src="../../assets/image/common/logo_color.svg" alt="" />
+			<img src="../../assets/image/common/logo_color.svg" alt="" />
 			</div>
 			<el-col>
 				<el-card shadow="hover">
-					<el-button type="text">DATA INSERT</el-button>
+					<el-button type="text" @click="goToDataInsert">DATA INSERT</el-button>
 				</el-card>
 			</el-col>
 			<el-col>
 				<el-card shadow="hover">
-					<el-button type="text">SEARCH</el-button>
+					<el-button type="text" @click="goToSearch">SEARCH</el-button>
 				</el-card>
 			</el-col>
 			<el-col>
 				<el-card shadow="hover">
-					<el-button type="text">Notice</el-button>
+					<el-button type="text" @click="goToNotice">Notice</el-button>
 				</el-card>
-			</el-col>						
+			</el-col>
 		</div>
 	</div>
 </template>
 
 <script>
+export default {
+	methods: {
+		goToDataInsert() {						
+			this.$router.push('/Insert');
+		},
+		goToSearch() {
+			this.$router.push('/SearchMain');
+		},
+		goToNotice() {
+			console.log('NoticeLIst 버튼 클릭됨');
+			this.$router.push('/NoticeLIst'); 			
+		}
+	}
+}
 </script>
-
-<style scoped>
-
-</style>
